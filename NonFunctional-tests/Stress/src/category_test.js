9c54658f3dbce392c22c6c11296b2ec9ac3964c7
nahu02 <nahu1002@gmail.com>
2024-05-06 21:11:36 +0200

    Category api stress testing

---
+++ NonFunctional-tests/Stress/src/category_test.js
@@ -0,0 +1,97 @@
+import http from 'k6/http';
+import { sleep, fail } from 'k6';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    stages: [
+        { duration: '30s', target: 5 },
+        { duration: '30s', target: 10 }, // this seems to be the most it can handle
+        { duration: '30s', target: 10 },
+        { duration: '30s', target: 10 },
+        { duration: '30s', target: 5 }
+    ]
+}
+
+// get the admin cookie, that can be used in request headers like:
+// 'Cookie': `token.AspNetCore.Cookies=${adminCookie}`
+function loginAsAdmin() {
+    let loginUrl = 'http://localhost:5100/api/User/login';
+
+    let payload = JSON.stringify({
+        email: 'admin@admin.com',
+        password: 'ADMIN_admin'
+    });
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    let res = http.post(loginUrl, payload, params);
+    if (res.status !== 200) {
+        fail("login failed with status " + res.status);
+    }
+    let adminCookie = res.cookies['.AspNetCore.Cookies'][0].value;
+    return adminCookie;
+}
+
+export default function () {
+    let baseUrl = 'http://localhost:5100/api/Category';
+    let adminCookie = loginAsAdmin();
+
+    // Create a new category
+    let payload = JSON.stringify({
+        name: "k6-" + Date.now()
+    });
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Cookie': `token.AspNetCore.Cookies=${adminCookie}`
+        },
+    };
+    let res = http.post(baseUrl, payload, params);
+    if (res.status !== 204) {
+        fail(`Category creation failed with status code: ${res.status}`);
+    }
+
+    sleep(1);
+
+
+    // List all categories
+    res = http.get(baseUrl);
+    if (res.status !== 200) {
+        fail(`Category listing failed with status code: ${res.status}`);
+    }
+
+    let randomCategoryId = res.json()[Math.floor(Math.random() * res.json().length)].id;
+
+    sleep(1);
+
+
+    // Get a category by id
+    res = http.get(`${baseUrl}/${randomCategoryId}`);
+    if (res.status !== 200) {
+        fail(`Category retrieval failed with status code: ${res.status}`);
+    }
+
+    sleep(1);
+
+
+    // Update a category
+    payload = JSON.stringify({
+        name: "k6-" + Date.now()
+    });
+    res = http.put(`${baseUrl}/${randomCategoryId}`, payload, params);
+    if (res.status !== 200) {
+        fail(`Category update failed with status code: ${res.status}`);
+    }
+
+    sleep(1);
+
+
+    // Delete a category
+    res = http.del(`${baseUrl}/${randomCategoryId}`, null, params);
+    if (res.status !== 202) {
+        fail(`Category deletion failed with status code: ${res.status}`);
+    }
+
+}
